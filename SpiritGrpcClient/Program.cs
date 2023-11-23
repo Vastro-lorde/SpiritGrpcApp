aa9f523ca6aef9b0392d37c53602aff9f0f0f9b1
Vastro-lorde <omatsolaseund@gmail.com>
2023-11-23 15:33:08 +0100

    Add project files.

---
+++ SpiritGrpcClient/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Grpc.Core;
+using Grpc.Net.Client;
+using SpiritGrpcServer;
+
+
+var input = new HelloRequest { Name = "Seun Daniel Omatsola" };
+var userDetail = new UserReqModel { Email = "firstname@email.com" };
+var channel = GrpcChannel.ForAddress("https://localhost:7242");
+var client = new Greeter.GreeterClient(channel);
+
+var userClient = new UserActions.UserActionsClient(channel);
+
+var user = await userClient.GetUserAsync(userDetail);
+var reply = await client.SayHelloAsync(input);
+
+Console.WriteLine(reply.Message);
+Console.WriteLine(user.Id);
+Console.WriteLine(user.Name);
+Console.WriteLine(user.Email);
+Console.WriteLine(user.Age);
+var uClient = userClient.GetAllUsers(new AllUsers { });
+    while (await uClient.ResponseStream.MoveNext())
+    {
+        var newUser = uClient.ResponseStream.Current;
+        Console.WriteLine($"Id: {newUser.Id}\nName: {newUser.Name}\nEmail: {newUser.Email}\nAge: {newUser.Age}\n\n");
+    }
+Console.ReadLine();
---
+++ SpiritGrpcServer/Services/UserService.cs
@@ -0,0 +1,89 @@
+﻿using Grpc.Core;
+using SpiritGrpcServer.Models;
+
+namespace SpiritGrpcServer.Services
+{
+    public class UserService : UserActions.UserActionsBase
+    {
+        private readonly ILogger<UserService> _logger;
+        private static List<UserModel> users = new List<UserModel>
+            {
+                new UserModel
+                {
+                    Id = "kjhjklh",
+                    Name = "First Name",
+                    Email = "firstname@email.com",
+                    Age = 35
+                },
+                new UserModel
+                {
+                    Id = "adfaewa",
+                    Name = "2 Name",
+                    Email = "secondname@email.com",
+                    Age = 33
+                },
+                new UserModel
+                {
+                    Id = "kkkeadx",
+                    Name = "3 Name",
+                    Email = "thirdname@email.com",
+                    Age = 35
+                },
+                new UserModel
+                {
+                    Id = "eiwofasd",
+                    Name = "4 Name",
+                    Email = "fourthname@email.com",
+                    Age = 45
+                },
+                new UserModel
+                {
+                    Id = "dakfdksd",
+                    Name = "5 Name",
+                    Email = "lastname@email.com",
+                    Age = 34
+                },
+            };
+
+        public UserService(ILogger<UserService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<UserResModel> GetUser(UserReqModel request, ServerCallContext context)
+        {
+            string requestEmail = request.Email != null? request.Email : "";
+            
+            UserModel user = users.FirstOrDefault((user) => user.Email == requestEmail);
+            if (user == null)
+            {
+                return Task.FromResult(new UserResModel());
+            }
+
+            return Task.FromResult(new UserResModel
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Email = user.Email,
+                Age = user.Age,
+            });
+        }
+
+        public override async Task GetAllUsers(AllUsers request, IServerStreamWriter<UserResModel> responseStream, ServerCallContext context)
+        {
+            var res = users;
+            foreach (var user in res)
+            {
+                var sendUser = new UserResModel
+                {
+                    Id = user.Id,
+                    Name = user.Name,
+                    Email = user.Email,
+                    Age = user.Age
+                };
+                await Task.Delay(1000);
+                await responseStream.WriteAsync(sendUser);
+            }
+        }
+    }
+}
